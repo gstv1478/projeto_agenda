959840b81509452f253e237904179cda48f5fcdb
gstv1478 <gstv1478@gmail.com>
2022-09-23 01:08:27 -0300

    first commit

---
+++ route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const route = express.Router();
+
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+
+
+// Rotas da home
+route.get('/', homeController.index);
+
+//rotas de login
+route.get('/login/index', loginController.index);
+
+
+module.exports = route;
